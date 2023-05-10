7e565c0bae129baf0382170406dd720bdb627d89
tarannum-2002 <tarannum.cs20@bmsce.ac.in>
2023-05-10 23:53:36 +0530

    sending and recieving text feature added

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-const io = require('socket.io-client');
+
 const socket = io('http://localhost:3000');
 
-socket.on('connect', () => {
-    console.log('Connected to server');
-    socket.emit('message', 'Hello from client!');
-});
+// socket.on('connect', () => {
+//     console.log('Connected to server');
+//     socket.emit('message', 'Hello from client!');
+// });
 
-socket.on('message', (data) => {
-    console.log(`Received message from server: ${data}`);
-});
+// socket.on('message', (data) => {
+//     console.log(`Received message from server: ${data}`);
+// });
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-});
+// socket.on('disconnect', () => {
+//     console.log('Disconnected from server');
+// });
+
+const messageForm = document.getElementById("send-container")
+const messageInput = document.getElementById("message-input")
+const messageContainer = document.getElementById("message-container")
+
+
+//asks name on the prompt
+//whateve name is given is broadcasted to everyone using emit function
+
+// 3 events form sever
+
+const name = prompt('What is your name?');
+appendMessage('You joined');
+socket.emit('new-user', name)
+//1. chat-message event
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+//2.user-connected
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+//user-disconnected
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = '';
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
--- server.js
+++ server.js
@@ -1,14 +1,50 @@
-const io = require('socket.io')(3000);
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
 
-    socket.on('message', (data) => {
-        console.log(`Received message from client: ${data}`);
-        socket.emit('message', `Server received message: ${data}`);
-    });
+// io.on('connection', (socket) => {
+//     console.log('Client connected');
+
+//     socket.on('message', (data) => {
+//         console.log(`Received message from client: ${data}`);
+//         socket.emit('message', `Server received message: ${data}`);
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('Client disconnected');
+//     });
+// });
+
+//list of users identified by their socked.id which is unique
+
+const users = {}
+
+
+
+
+// ...
+
+
+io.on('connection', socket => {
+    console.log("new client")
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name)  //connected users name is broadcasted to everyone
+    })
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message',
+            { message, name: users[socket.id] }) //emit messages to everyone 
+    })
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+        socket.broadcast.emit('user-disconnected', users[socket.id])  //disconncted emit message to add 
+        delete users[socket.id]
+    })
 });
